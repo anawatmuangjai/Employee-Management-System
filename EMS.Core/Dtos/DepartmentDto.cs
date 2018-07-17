3253944529dc38fa31d755b94df169df1d325206
Anawat Muangjai <anawat.muangjai@gmail.com>
2018-07-17 16:26:12 +0700

    Add data tranfer object and repository interfaces

---
+++ EMS.Core/Dtos/DepartmentDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EMS.Core.Dtos
+{
+    public class DepartmentDto
+    {
+        public int DepartmentID { get; set; }
+        public string DepartmentName { get; set; }
+        public string DepartmentCode { get; set; }
+        public string DepartmentGroup { get; set; }
+    }
+}
--- EMS.Core/Interfaces/IDepartmentRepository.cs
+++ EMS.Core/Interfaces/IDepartmentRepository.cs
@@ -1,10 +1,19 @@
-﻿using System;
+﻿using EMS.Core.Dtos;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace EMS.Core.Interfaces
 {
     public interface IDepartmentRepository
     {
+        IEnumerable<DepartmentDto> GetAll();
+        IEnumerable<DepartmentDto> GetByName(string name);
+        DepartmentDto GetById(int id);
+        DepartmentDto Add(DepartmentDto department);
+        void Update(DepartmentDto department);
+        void Delete(DepartmentDto department);
     }
 }
