05c2d15964e5768bf9fd4437cd33254391135b30
Anawat Muangjai <anawat.muangjai@gmail.com>
2018-07-17 17:19:48 +0700

    Add repository

--- EMS.Infrastructure/Repositories/EmployeeRepository.cs
+++ EMS.Infrastructure/Repositories/EmployeeRepository.cs
@@ -1,9 +1,11 @@
 ﻿using EMS.Core.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
-namespace EMS.Infrastructure.Repository
+namespace EMS.Infrastructure.Repositories
 {
     public class EmployeeRepository : IEmployeeRepository
     {
