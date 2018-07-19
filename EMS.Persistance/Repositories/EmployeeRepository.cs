ad8d5e3914b412ad9a2bfe9f45688666b33a9d93
Anawat Muangjai <anawat.muangjai@gmail.com>
2018-07-19 23:58:48 +0700

    Add employee repository

---
+++ EMS.Persistance/Repositories/EmployeeRepository.cs
@@ -0,0 +1,32 @@
+﻿using EMS.ApplicationCore.Interfaces.Repositories;
+using EMS.Domain.Entities;
+using EMS.Persistance.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EMS.Persistance.Repositories
+{
+    public class EmployeeRepository : Repository<Employee>, IEmployeeRepository
+    {
+        public EmployeeRepository(EmployeeContext dbContext) 
+            : base(dbContext)
+        {
+        }
+
+        public async Task<List<Employee>> GetEmployeeListAsync()
+        {
+            var result = await _dbContext.Employee
+                .Include(image => image.EmployeeImage)
+                .Include(state => state.EmployeeState)
+                    .ThenInclude(job => job.Job)
+                .Include(state => state.EmployeeState)
+                    .ThenInclude(dept => dept.Department)
+                .ToListAsync();
+
+            return result;
+        }
+    }
+}
