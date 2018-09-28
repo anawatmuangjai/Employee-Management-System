26da667474605afdff61690f0727d11bcd1e16f0
Anawat Muangjai <anawat.muangjai@gmail.com>
2018-09-28 17:16:56 +0700

    Add model class dashboard data

---
+++ EMS.WebCore/Models/Dashboard/AttendanceShift.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EMS.WebCore.Models.Dashboard
+{
+    public class AttendanceShift
+    {
+        public string ShiftName { get; set; }
+        public int TotalEmployee { get; set; }
+        public int ActiveEmployee { get; set; }
+    }
+}
