41a2684207d23aecd2ba1eed53a9c512ed0df801
Anawat Muangjai <anawat.muangjai@gmail.com>
2018-10-19 16:42:25 +0700

    Update function skill mapping

--- EMS.WebCore/Controllers/SkillMappingController.cs
+++ EMS.WebCore/Controllers/SkillMappingController.cs
@@ -12,29 +12,48 @@ namespace EMS.WebCore.Controllers
     {
         private readonly IEmployeeService _employeeService;
         private readonly IEmployeeSkillService _employeeSkillService;
+        private readonly IEmployeeImageService _employeeImageService;
 
         public SkillMappingController(
-            IEmployeeService employeeService, 
-            IEmployeeSkillService employeeSkillService)
+            IEmployeeService employeeService,
+            IEmployeeSkillService employeeSkillService,
+            IEmployeeImageService employeeImageService)
         {
             _employeeService = employeeService;
             _employeeSkillService = employeeSkillService;
+            _employeeImageService = employeeImageService;
         }
 
         [HttpGet]
         [HttpPost]
-        public async Task<IActionResult> Index(string firstEmployeeId,string secondEmployeeId)
+        public async Task<IActionResult> Index(string firstEmployeeId, string secondEmployeeId)
         {
             var viewModel = new SkillMappingViewModel();
 
             if (!string.IsNullOrEmpty(firstEmployeeId))
             {
+                var image = await _employeeImageService.GetByEmployeeId(firstEmployeeId);
+
+                if (image != null)
+                {
+                    var imageBase64Data = Convert.ToBase64String(image.Images);
+                    viewModel.FirstProfileImage = string.Format("data:image/png;base64,{0}", imageBase64Data);
+                }
+
                 viewModel.FirstEmployee = await _employeeService.GetByEmployeeIdWithDetailAsync(firstEmployeeId);
                 viewModel.FirstEmployeeSkills = await _employeeSkillService.GetByEmployeeId(firstEmployeeId);
             }
 
             if (!string.IsNullOrEmpty(secondEmployeeId))
             {
+                var image = await _employeeImageService.GetByEmployeeId(secondEmployeeId);
+
+                if (image != null)
+                {
+                    var imageBase64Data = Convert.ToBase64String(image.Images);
+                    viewModel.SecondProfileImage = string.Format("data:image/png;base64,{0}", imageBase64Data);
+                }
+
                 viewModel.SecondEmployee = await _employeeService.GetByEmployeeIdWithDetailAsync(firstEmployeeId);
                 viewModel.SecondEmployeeSkills = await _employeeSkillService.GetByEmployeeId(firstEmployeeId);
             }
--- EMS.WebCore/ViewModels/SkillMapping/SkillMappingViewModel.cs
+++ EMS.WebCore/ViewModels/SkillMapping/SkillMappingViewModel.cs
@@ -8,6 +8,8 @@ namespace EMS.WebCore.ViewModels.SkillMapping
 {
     public class SkillMappingViewModel
     {
+        public string FirstProfileImage { get; set; }
+        public string SecondProfileImage { get; set; }
         public EmployeeModel FirstEmployee { get; set; }
         public EmployeeModel SecondEmployee { get; set; }
         public IEnumerable<EmployeeSkillModel> FirstEmployeeSkills { get; set; }
